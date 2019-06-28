8b64308b5ac83a42b3ead14cceafdb4be5f825b4
joran <joran@decagon.com>
2019-06-27 20:18:03 -0700

    fix version and classifiers

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name='bokeh_wordcloud2',
-    version='1.0',
+    version='0.9',
     packages=['bokeh_wordcloud2'],
     package_data={
         'bokeh_wordcloud2':['typescript/extension_bokeh_wordcloud2.ts'],
@@ -16,5 +16,16 @@ setup(
     author='joran beasley',
     install_requires=['bokeh>=1.1.0'],
     author_email='joranbeasley@gmail.com',
-    description='a bokeh extension that implements [wordcloud2](https://wordcloud2-js.timdream.org)'
+    description='a bokeh extension that implements a wordcloud (https://wordcloud2-js.timdream.org)',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Other Audience",
+        "License :: Freely Distributable",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Widget Sets"
+    ]
 )
